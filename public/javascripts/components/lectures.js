5b0e1e63701127837f1bed237bc2d7c31dede63a
Shaohua Huang <hshnju@gmail.com>
2015-12-14 14:19:55 +0800

    change

--- public/javascripts/components/lectures.js
+++ public/javascripts/components/lectures.js
@@ -347,6 +347,6 @@ function getInitState(){
         lectureLeftArray.push('');
         lectureRightArray.push('');
     }
-    console.log('It is initiated successfully ------test1!');
+    console.log('It is initiated successfully ------test1&2!');
     return {lectureLeftArray, lectureRightArray};
 }
